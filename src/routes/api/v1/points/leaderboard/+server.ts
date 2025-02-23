ca69a4969afff9597bd0a655de3972a315312de9
ByteAtATime <byteatatime@proton.me>
2025-02-22 20:51:36 -0800

    fix: update leaderboard endpoint to use points service

--- src/routes/api/v1/points/leaderboard/+server.ts
+++ src/routes/api/v1/points/leaderboard/+server.ts
@@ -1,5 +1,5 @@
 import { compose } from '$lib/server/endpoints';
-import { withPointsRepo } from '$lib/server/endpoints/dependencies';
+import { withPointsService } from '$lib/server/endpoints/dependencies';
 import { endpoint_GET } from './endpoint';
 
-export const GET = compose(withPointsRepo())(endpoint_GET);
+export const GET = compose(withPointsService())(endpoint_GET);
