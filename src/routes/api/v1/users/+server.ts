604feb0a9a314d738a8ca4d36db8600b49f8d000
ByteAtATime <byteatatime@proton.me>
2025-02-28 10:26:53 -0800

    refactor: update GET handler to use user service and query schema

--- src/routes/api/v1/users/+server.ts
+++ src/routes/api/v1/users/+server.ts
@@ -1,6 +1,9 @@
-import { compose } from '$lib/server/endpoints';
-import { withAuthProvider } from '$lib/server/endpoints/dependencies';
-import { endpoint_GET } from './endpoint';
+import { compose, withQuerySchema } from '$lib/server/endpoints';
+import { withUserService } from '$lib/server/endpoints/dependencies';
+import { endpoint_GET, querySchema } from './endpoint';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = compose(withAuthProvider())(endpoint_GET);
+export const GET: RequestHandler = compose(
+	withUserService(),
+	withQuerySchema(querySchema)
+)(endpoint_GET);
